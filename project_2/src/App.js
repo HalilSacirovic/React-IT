a4a307d4261bffe3697593edbda8d9c4c271c7f3
HalilSacirovic <halilsacirovic03@gmail.com>
2023-05-09 23:44:53 +0200

    first try and it is working  Project_two

--- project_2/src/App.js
+++ project_2/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from './Components/Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         test number uno
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state={
+        home:0,
+        quest:0,
+        gameover:false,
+    }
+
+componentDidMount(){
+    console.log("Mount");
+}
+
+componentDidUpdate(){
+    console.log("Update");
+    if(this.state.gameover === false){
+        if(this.state.home === 5){
+            this.setState({gameover:true})
+        }
+        else if(this.state.quest === 5){
+            this.setState({gameover:true})
+        }
+    
+    }
+}
+    render() {
+        return (
+        
+        <div className="App">
+            <div className='nav'>
+                <Card 
+                name={"Home"}
+                team={this.state.home}
+                goal={()=>{
+                    if(this.state.gameover=== false){
+                        this.setState({home:this.state.home+1})
+                    }
+                }} />
+                <Card 
+                name={"Guest"}
+                team={this.state.quest}
+                goal={()=>{
+                    if(this.state.gameover === false) {
+
+                        this.setState({quest:this.state.quest+1})
+                    }
+                }} />
+               
+                
+            </div>
+        </div>
+        )
+
+
+
+    }
 }
 
 export default App;
+
