213ed7c1b68c641e32b1c684903ce2600e2ce816
HalilSacirovic <halilsacirovic03@gmail.com>
2023-06-07 20:51:15 +0200

    login formik

--- project_9/src/App.js
+++ project_9/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes/AllQuotes";
 import QuoteDetails from "./pages/QuoteDetails/QuoteDetails";
 import EditQuote from "./pages/QuoteDetails/EditQuote";
+import Login from "./Login/Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<AllQuotes />} />
         <Route path="/quote/:id" element={<QuoteDetails />} />
         <Route path="/quote/edit/:id" element={<EditQuote/>} />
+        <Route path="/login" element ={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
