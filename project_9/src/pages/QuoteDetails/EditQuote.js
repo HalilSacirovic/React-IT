3730e3641835f4bb1c1d9ca760d47dd87325c940
HalilSacirovic <halilsacirovic03@gmail.com>
2023-06-07 19:14:43 +0200

    radiiiiii

--- project_9/src/pages/QuoteDetails/EditQuote.js
+++ project_9/src/pages/QuoteDetails/EditQuote.js
@@ -1,6 +1,6 @@
 import React, { useEffect,useState } from "react";
 import "./QuoteDetails.css";
-import {  useParams } from 'react-router-dom';
+import { useSearchParams, useParams, useNavigate, Link } from "react-router-dom";
 
 const EditQuote = () => {
 
@@ -9,15 +9,26 @@ const EditQuote = () => {
     const [editQuote,setEditQuote] = useState("");
     const [editSource,setEditSource] = useState("");
 
+
     const params = useParams();
 
-    useEffect(() => {
+
+  
+      const EditData = {
+        quoteText : editQuote,
+        quoteAuthor:editAuthor,
+        quoteSource:editSource,
+      }
+
+      const navigate = useNavigate();
+    
+
+      let TryEdit =()=>{
+
+    
         fetch("https://js-course-server.onrender.com/quotes/edit/"+ params.id, {
             method: "PATCH",
-                body: JSON.stringify({
-                    quoteText : editQuote,
-
-                }),
+                body: JSON.stringify(EditData),
                 headers: {
                 "Content-Type": "application/json",
           },
@@ -26,33 +37,52 @@ const EditQuote = () => {
             return res.json();
           })
           .then((result) => {
-           //////////////////////
-           //////////////////
-           ///////////////////
+
+            alert("valjda uspesno??");
+            navigate("/")
+
           })
           .catch((error) => {
             console.log("Error", error);
           });
-      }, []);
+
+        }
+
 
   return (
    
     <div>
 
         <div>
-            <input value={""} placeholder='Author'></input>
-            <input placeholder='Quotes'></input>
-            <input placeholder='Likes'></input>
+            <input  value={editAuthor}
+              onChange={(e) => {
+                setEditAuthor(e.target.value);
+              }}
+               placeholder='Author'></input>
+            <input
+             value={editQuote}
+             onChange={(e) => {
+               setEditQuote(e.target.value);
+             }}
+            placeholder='Quotes'></input>
+            <input 
+               value={editSource}
+               onChange={(e) => {
+                 setEditSource(e.target.value);
+                 console.log(e.target.value)
+               }}
+            placeholder='Likes'></input>
 
-            <button onClick={(
-                ////
-            )=>{
-
-            }} >Edituj</button>
+           <button onClick={()=>{
+            TryEdit();
+           }}>
+            Probaj da editujes
+           </button>
         </div>
 
     </div>
   )
-}
+  }
+
 
 export default EditQuote;
